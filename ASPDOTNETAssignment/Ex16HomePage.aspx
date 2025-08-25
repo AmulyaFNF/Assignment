5d62c4f809f5fa59cb10e70308de82b381118fc5
AmulyaFNF <amulyabn448@gmail.com>
2025-08-25 19:52:48 +0530

    Add files via upload

---
+++ ASPDOTNETAssignment/Ex16HomePage.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Ex16HomePage.aspx.cs" Inherits="SampleWebFormsApp.Ex16HomePage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="mainContent" runat="server">
+    <h1>HomePage</h1>
+    <asp:Button Text="Login" ID="btnLogin" runat="server" OnClick="btnLogin_Click" />
+    <asp:Button Text="Register" ID="btnRegister" runat="server" OnClick="btnRegister_Click" />
+</asp:Content>
---
+++ ASPDOTNETAssignment/Ex16HomePage.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SampleWebFormsApp
+{
+    public partial class Ex16HomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Ex14LoginPage.aspx");
+        }
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Ex13ValidationControls.aspx");
+        }
+    }
+}
